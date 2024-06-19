eed66f5d06ee3c4105218edc8396ec87249fa4ce
Heru Handika <herubiolog@gmail.com>
2024-06-19 18:40:08 -0500

    Allow users to input from dir or list of files.

--- src/align/concat.rs
+++ src/align/concat.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use pyo3::prelude::*;
 use segul::{
@@ -9,26 +9,59 @@ use segul::{
     },
 };
 
-#[pyfunction]
-pub(crate) fn concat_alignments(
-    input_dir: &str,
-    input_fmt: &str,
-    datatype: &str,
-    output_prefix: &str,
-    output_fmt: &str,
-    partition_fmt: &str,
-) {
-    let fmt = input_fmt.parse::<InputFmt>().expect("Invalid input format");
-    let dir = Path::new(input_dir);
-    let output = Path::new(output_prefix);
-    let datatype = datatype.parse::<DataType>().expect("Invalid data type");
-    let out_fmt = output_fmt
-        .parse::<OutputFmt>()
-        .expect("Invalid output format");
-    let part_fmt = partition_fmt
-        .parse::<PartitionFmt>()
-        .expect("Invalid partition format");
-    let mut files = SeqFileFinder::new(dir).find(&fmt);
-    let mut handle = ConcatHandler::new(&fmt, &output, &out_fmt, &part_fmt);
-    handle.concat_alignment(&mut files, &datatype);
+#[pyclass]
+pub struct AlignmentConcatenation {
+    input_files: Vec<PathBuf>,
+    input_fmt: InputFmt,
+    datatype: DataType,
+    output_prefix: String,
+    output_fmt: OutputFmt,
+    partition_fmt: PartitionFmt,
+}
+
+#[pymethods]
+impl AlignmentConcatenation {
+    #[new]
+    fn new(
+        input_fmt: &str,
+        datatype: &str,
+        output_prefix: &str,
+        output_fmt: &str,
+        partition_fmt: &str,
+    ) -> Self {
+        Self {
+            input_files: Vec::new(),
+            input_fmt: input_fmt.parse::<InputFmt>().expect("Invalid input format"),
+            datatype: datatype.parse::<DataType>().expect("Invalid data type"),
+            output_prefix: output_prefix.to_string(),
+            output_fmt: output_fmt
+                .parse::<OutputFmt>()
+                .expect("Invalid output format"),
+            partition_fmt: partition_fmt
+                .parse::<PartitionFmt>()
+                .expect("Invalid partition format"),
+        }
+    }
+
+    #[setter]
+    pub(crate) fn input_from_files(&mut self, input_files: Vec<String>) {
+        self.input_files = input_files.iter().map(PathBuf::from).collect();
+    }
+
+    #[setter]
+    pub(crate) fn input_from_dir(&mut self, input_dir: &str) {
+        let input_dir = Path::new(input_dir);
+        self.input_files = SeqFileFinder::new(input_dir).find(&self.input_fmt);
+    }
+
+    pub(crate) fn concat_alignments(&mut self) {
+        let output = Path::new(&self.output_prefix);
+        let mut handle = ConcatHandler::new(
+            &self.input_fmt,
+            output,
+            &self.output_fmt,
+            &self.partition_fmt,
+        );
+        handle.concat_alignment(&mut self.input_files, &self.datatype);
+    }
 }
--- src/align/mod.rs
+++ src/align/mod.rs
@@ -6,13 +6,13 @@ mod summary;
 
 use pyo3::prelude::*;
 
-use crate::align::concat::concat_alignments;
+use crate::align::concat::AlignmentConcatenation;
 use crate::align::convert::convert_alignments;
 use crate::align::filter::AlignmentFiltering;
 use crate::align::summary::summarize_alignments;
 
 pub(crate) fn register(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(concat_alignments, m)?)?;
+    m.add_class::<AlignmentConcatenation>()?;
     m.add_function(wrap_pyfunction!(summarize_alignments, m)?)?;
     m.add_function(wrap_pyfunction!(convert_alignments, m)?)?;
     m.add_class::<AlignmentFiltering>()?;
