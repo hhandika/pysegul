0c13e62ff68511bb1dadf801c969c990e91fb89e
Heru Handika <herubiolog@gmail.com>
2024-06-27 13:18:08 -0500

    Fix file naming issues.

--- src/align/partition.rs
+++ src/align/partition.rs
@@ -3,7 +3,10 @@ use std::path::PathBuf;
 use pyo3::prelude::*;
 use segul::{
     handler::align::partition::PartConverter,
-    helper::types::{DataType, PartitionFmt},
+    helper::{
+        partition::construct_partition_path,
+        types::{DataType, PartitionFmt},
+    },
 };
 
 use crate::common::SEQ_DATA_TYPE_ERR;
@@ -14,7 +17,7 @@ use super::PARTITION_FMT_ERR;
 pub(crate) struct PartitionConversion {
     input_fmt: PartitionFmt,
     datatype: DataType,
-    output_path: PathBuf,
+    output_dir: PathBuf,
     output_format: PartitionFmt,
     check_partition: bool,
 }
@@ -25,31 +28,34 @@ impl PartitionConversion {
     pub(crate) fn new(
         input_fmt: &str,
         datatype: &str,
-        output_path: &str,
+        output_dir: &str,
         output_fmt: &str,
         check_partition: bool,
     ) -> Self {
         Self {
             input_fmt: input_fmt.parse::<PartitionFmt>().expect(PARTITION_FMT_ERR),
             datatype: datatype.parse::<DataType>().expect(SEQ_DATA_TYPE_ERR),
-            output_path: PathBuf::from(output_path),
+            output_dir: PathBuf::from(output_dir),
             output_format: output_fmt.parse::<PartitionFmt>().expect(PARTITION_FMT_ERR),
             check_partition,
         }
     }
 
     pub(crate) fn from_files(&self, input_files: Vec<String>) {
-        input_files
-            .iter()
-            .map(PathBuf::from)
-            .for_each(|f| self.convert_partitions(f))
+        input_files.iter().map(PathBuf::from).for_each(|f| {
+            let output_path = self
+                .output_dir
+                .join(f.file_name().expect("Invalid file name"));
+            let final_path = construct_partition_path(&output_path, &self.output_format);
+            self.convert_partitions(f, final_path)
+        })
     }
 
-    fn convert_partitions(&self, input_path: PathBuf) {
+    fn convert_partitions(&self, input_path: PathBuf, output_path: PathBuf) {
         let handle = PartConverter::new(
             &input_path,
             &self.input_fmt,
-            &self.output_path,
+            &output_path,
             &self.output_format,
         );
         handle.convert(&self.datatype, self.check_partition)
--- tests/test_align_partition.py
+++ tests/test_align_partition.py
@@ -1,16 +1,18 @@
 import pysegul
 
+from os import listdir
+
 def test_partition_convert(tmp_path):
     input_paths = [
-        'tests\partition-data\partition_codon.txt', 
-        'tests\partition-data\partition.txt'
+        'tests/partition-data/partition_codon.txt', 
+        'tests/partition-data/partition.txt'
         ]
     input_format = 'raxml'
     output_format = 'nexus'
     datatype = 'dna'
     check_partition = True
     output_dir = str(tmp_path.joinpath('results'))
-    convert = pysegul.PartitionConvert(
+    convert = pysegul.PartitionConversion(
         input_format,
         datatype,
         output_dir,
@@ -18,3 +20,8 @@ def test_partition_convert(tmp_path):
         check_partition
         )
     convert.from_files(input_paths)
+    assert tmp_path.joinpath('results').exists()
+    # Check if the output directory contains the expected files
+    results = listdir(output_dir)
+    assert len(results) == 2
+    assert 'partition_codon_partition.nex' in results
